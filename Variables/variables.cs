0aaf5ce11d788772a77f99c439b5bf8837342654
RhexNiebres <rhexniebres@gmail.com>
2025-08-09 12:03:28 +0800

    chore add explicit and implicit variable examples

--- Variables/variables.cs
+++ Variables/variables.cs
@@ -25,3 +25,11 @@ float floatA = 1.2F;// handles numeric value with decimal places
 double doubleA = 1.2D;// used for measurement and weight and number with decimal places except for money
 
 decimal decimalA = 1.2M; // used for when data is money or currency 
+
+//----------------------
+
+//Locale variables
+//explicit - you tell it. It includes the  <data type> <name of variable>
+int numberA = 1;
+//implicit - C# guesses (but still type-safe). It only knows the data type when you put the value
+var numberB = 1;
